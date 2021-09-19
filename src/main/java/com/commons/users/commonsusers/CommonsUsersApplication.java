e17d385393297d9b0049c4f3a3e93cc7c8264635
Kevin Perez <kevinp@cronapis.com>
2021-09-19 14:16:50 -0400

    first commit

---
+++ src/main/java/com/commons/users/commonsusers/CommonsUsersApplication.java
@@ -0,0 +1,12 @@
+package com.commons.users.commonsusers;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class CommonsUsersApplication {
+
+
+}
---
+++ src/test/java/com/commons/users/commonsusers/CommonsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commons.users.commonsusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
